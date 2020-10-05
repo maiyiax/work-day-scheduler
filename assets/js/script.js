e1ced7082f93b967933f936c58e902d8da2276c7
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-04 23:59:19 -0500

    fixed bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,15 +33,16 @@ $( document ).ready(function() {
         console.log(eventText);
         console.log(eventTime);
         localStorage.setItem(eventTime, eventText);
-  });
+    });
 
-// retrieve info from localStorage
-$("#9 .description").val(localStorage.getItem("9"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#13 .description").val(localStorage.getItem("13"));
-$("#14 .description").val(localStorage.getItem("14"));
-$("#15 .description").val(localStorage.getItem("15"));
-$("#16 .description").val(localStorage.getItem("16"));
-$("#17 .description").val(localStorage.getItem("17"));
+    // retrieve info from localStorage
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+});

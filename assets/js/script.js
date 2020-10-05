1f41135777c16c7993e0ef6f64eb9ff926e1a4e4
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-04 19:33:55 -0500

    added function to check timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,66 @@
 // create a function to display current date in the header
 var currentDate = moment();
-$("#currentDay")
-    .text(currentDate
-    .format("ddd, MMMM Do, YYYY")
-    );
-console.log(currentDate);
+$("#currentDay").text(currentDate.format("ddd, MMMM Do, YYYY"));
+
+// variable for event per time block
+var events = {
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+};
+
+// determine current hour block
+var time = currentDate.hour();
+console.log(time);
+
+// check if block of hour is in the past, present or future of current time
+$(".row").each(function() {
+    var selectedHour = parseInt($(this).attr("id"));
+    console.log(selectedHour);
+
+    if (selectedHour < time) {
+        $(this).addClass("past");
+    }
+    else if (selectedHour === time) {
+        $(this)
+            .removeClass("past")
+            .addClass("present");
+    }
+    else {
+        $(this)
+            .removeClass("past", "present")
+            .addClass("future");
+    }
+    console.log($(this));
+});
+
+
+// color-code timeblocks to indicate whether it is in the past, present, or future
+
+
+// save events to localstorage
+$("event").click(function() {
+    var eventText = $(this)
+        .text()
+        .trim();
+
+    console.log(eventText);
+})
+
+// click listener for save button
+$(".saveBtn")
+    .click(function() {
+        localStorage.setItem("event", JSON.stringify(event))
+    console.log($(this));
+});
+
+//var saveEvents = function () {
+ //   localStorage.setItem("events", JSON.stringify(events);
+   // console.log(saveEvents);
+// };
